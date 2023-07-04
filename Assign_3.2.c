180420188613ce6c293bfe0166325cca4e1b9820
atgAtharvGupta <138558250+atgAtharvGupta@users.noreply.github.com>
2023-07-04 12:14:07 +0530

    Update Assign_3.2.c

--- Assign_3.2.c
+++ Assign_3.2.c
@@ -1,4 +1,4 @@
-//Amicable numbers are found in pairs. A given pair of numbers is Amicable if the sum of the proper divisors (not including itself) of one number is equal to the other number and vice – versa. For example 220 & 284 are amicable numbers First we find the proper divisors of 220: 220:1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110
+//Amicable numbers are found in pairs. A given pair of numbers is Amicable if the sum of the proper divisors (not including itself) of one number is equal to the other number and vice â€“ versa. For example 220 & 284 are amicable numbers First we find the proper divisors of 220: 220:1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110
 //1+ 2 + 4 + 5 + 10 + 11 + 20 + 22 + 44 + 55 + 110 = 284 Now, 284: 1, 2, 4, 71,142 1 + 2 + 4 + 71 + 142 = 220 Write a function to check that the input pair of numbers is amicable.
 
 #include<stdio.h>
